ffb68e5cf680fab1e5f109060f076a551f2f7c54
KD <kdkahlo@gmail.com>
2025-01-10 10:34:52 +0100

    working with inputs test completed, testing sign in buttons & login inputs.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -14,23 +14,30 @@ test('Clicking on elements', async ({page}) => {
     const errorMessage = await page.locator('.alert-error')
     await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
-test('Selectors', async ({page}) => {
-//text
-await page.click('text = Select')
-//CSS selectors
-await page.click('button')
-//ID selectors
-await page.click('#id')
-//class selectors
-await page.click('.class')
+// test('Selectors', async ({page}) => {
+// //text
+// await page.click('text = Select')
+// //CSS selectors
+// await page.click('button')
+// //ID selectors
+// await page.click('#id')
+// //class selectors
+// await page.click('.class')
+// //Only visible CSS selectors
+// await page.click('.submit-button:visible')
+// //combination in selector to be more precise in testing, targeting better performance
+// await page.click('#username .first-name')
+// //XPath
+// await page.click('//button')
+// })
+test('Working with Inputs', async  ({page}) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
 
-//Only visible CSS selectors
-await page.click('.submit-button:visible')
+    await page.fill('#user_login', 'some username')
+    await page.fill('#user_password', 'some password')
 
-//combination in selector to be more precise in testing, targeting better performance
-await page.click('#username .first-name')
-
-//XPath
-await page.click('//button')
-
+    await page.click('text=Sign in')
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
