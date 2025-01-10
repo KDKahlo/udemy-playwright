b9ee6d8407af6f0b0b0dd932a1ce01e00fa7f311
KD <kdkahlo@gmail.com>
2025-01-10 10:19:34 +0100

    test with sample templates for selectors

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -14,3 +14,23 @@ test('Clicking on elements', async ({page}) => {
     const errorMessage = await page.locator('.alert-error')
     await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
+test('Selectors', async ({page}) => {
+//text
+await page.click('text = Select')
+//CSS selectors
+await page.click('button')
+//ID selectors
+await page.click('#id')
+//class selectors
+await page.click('.class')
+
+//Only visible CSS selectors
+await page.click('.submit-button:visible')
+
+//combination in selector to be more precise in testing, targeting better performance
+await page.click('#username .first-name')
+
+//XPath
+await page.click('//button')
+
+})
